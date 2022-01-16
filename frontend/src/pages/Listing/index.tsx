40420b4ad1b66c907cfcd4d5dbef22b67bbb17ae
Filipe Aguiar <filipesag@hotmail.com>
2022-01-16 20:45:18 -0300

    First request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,7 +1,18 @@
+import axios from "axios";
 import MovieCard from "components/moviecard";
 import Pagination from "components/pagination";
+import { BASE_URL } from "utils/requests";
 
 function Listing() {
+
+    axios.get(`${BASE_URL}/movies?size=12&page=0`)
+    .then(response => {
+        console.log(response.data)
+    });
+
+
+
+
     return (
         <>
             <Pagination />
