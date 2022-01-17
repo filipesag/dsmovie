35b56466180f34cff2050bc1b3685c02b86501e4
Filipe Aguiar <filipesag@hotmail.com>
2022-01-16 22:35:57 -0300

    Show score

--- frontend/src/components/moviescore/index.tsx
+++ frontend/src/components/moviescore/index.tsx
@@ -1,19 +1,21 @@
 import MovieStars from "components/moviestars";
 import './style.css';
 
-function MovieScore() {
+type Props = {
+    score: number;
+    count: number;
+}
 
-    const score = 3.5;
-    const count = 13;
+function MovieScore({ score, count }: Props) {
 
-    return(
-        <div className = "dsmovie-score-container" >
+    return (
+        <div className="dsmovie-score-container" >
             <p className="dsmovie-score-value">{score > 0 ? score.toFixed(1) : '-'}</p>
-            <MovieStars />
+            <MovieStars score={score} />
             <p className="dsmovie-score-count">{count} avaliações</p>
         </div>
     );
 }
 
 export default MovieScore;
-export {};
+export { };
